3560b9407bb3d8a3f9e7b582830dcb4d013c3541
Julian Lawrance <jl2468@bath.ac.uk>
2019-07-03 19:30:05 +0100

    Create type chart control
    It's a grid with one column for each type and one row for each Pokemon,
    plus extra rows above and below

---
+++ PokePlanner/Controls/TypeChart.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.ComponentModel;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using PokePlanner.Mechanics;
+using Type = PokePlanner.Mechanics.Type;
+
+namespace PokePlanner.Controls
+{
+    /// <summary>
+    /// Interaction logic for TypeChart.xaml.
+    /// </summary>
+    public partial class TypeChart
+    {
+        /// <summary>
+        /// Create type columns.
+        /// </summary>
+        public TypeChart()
+        {
+            InitializeComponent();
+
+            if (!DesignerProperties.GetIsInDesignMode(this))
+            {
+                CreateTypeColumns();
+            }
+        }
+
+        /// <summary>
+        /// Creates columns in the grid for each type.
+        /// </summary>
+        private void CreateTypeColumns()
+        {
+            var types = Enum.GetValues(typeof(Type)).Cast<Type>().Select(t => t.ToString()).ToArray();
+            for (var i = 0; i < types.Length; i++)
+            {
+                // create column for type
+                grid.ColumnDefinitions.Add(new ColumnDefinition
+                {
+                    MinWidth = 60
+                });
+
+                // create label for type
+                var type = types[i];
+                var label = new Label
+                {
+                    Content = type,
+                    HorizontalContentAlignment = HorizontalAlignment.Center,
+                    VerticalContentAlignment = VerticalAlignment.Center,
+                    Background = Types.Instance.GetTypeBrush(type)
+                };
+
+                // add label to grid's header row
+                Grid.SetRow(label, 0);
+                Grid.SetColumn(label, i);
+                grid.Children.Add(label);
+            }
+        }
+    }
+}
