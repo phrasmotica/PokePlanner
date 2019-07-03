5311376ce6d177c9f3814c750f04db79a8273e27
Julian Lawrance <jl2468@bath.ac.uk>
2019-07-03 14:55:18 +0100

    Create extension method class

---
+++ PokePlanner/Util/Ext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace PokePlanner.Util
+{
+    /// <summary>
+    /// Support class for extension methods.
+    /// </summary>
+    public static class Ext
+    {
+        /// <summary>
+        /// Returns the string as an enum value.
+        /// </summary>
+        public static T ToEnum<T>(this string st)
+        {
+            if (string.IsNullOrEmpty(st))
+            {
+                return default(T);
+            }
+
+            return (T) Enum.Parse(typeof(T), st, true);
+        }
+    }
+}
