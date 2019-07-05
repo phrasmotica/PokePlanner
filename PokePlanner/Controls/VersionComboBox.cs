d4124e1cccf4671c6b62b7d63d3b790b724dfbf1
Julian Lawrance <julian.lawrance@gmail.com>
2019-07-05 21:37:32 +0100

    Create combo box for selecting a version group

---
+++ PokePlanner/Controls/VersionComboBox.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using PokeAPI;
+
+namespace PokePlanner.Controls
+{
+    /// <summary>
+    /// Combo box for displaying version groups.
+    /// </summary>
+    public class VersionComboBox : ComboBox
+    {
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public VersionComboBox()
+        {
+            LoadVersionGroupData();
+        }
+
+        /// <summary>
+        /// Loads version groups into the combo box.
+        /// </summary>
+        public async void LoadVersionGroupData()
+        {
+            var groups = await GetVersionGroups();
+            ItemsSource = groups.Select(g => g.Name);
+        }
+
+        /// <summary>
+        /// Returns all version groups from PokeAPI.
+        /// </summary>
+        public static async Task<ResourceList<NamedApiResource<VersionGroup>, VersionGroup>> GetVersionGroups()
+        {
+            Console.WriteLine(@"Getting version groups...");
+            var versionGroups = await DataFetcher.GetResourceList<NamedApiResource<VersionGroup>, VersionGroup>();
+            Console.WriteLine($@"Got {versionGroups.Count} version groups.");
+
+            var groupList = versionGroups.ToList();
+            Console.WriteLine($@"Got {groupList.Count} version groups:");
+            for (var i = 0; i < groupList.Count; i++)
+            {
+                var group = groupList[i];
+                Console.WriteLine(i + " " + group.Name);
+            }
+
+            return versionGroups;
+        }
+    }
+}
