7356ba5a52c804f843b28eb27af3e130d095c142
Julian Lawrance <julian.lawrance@gmail.com>
2019-07-05 17:42:04 +0100

    Create control for choosing options

---
+++ PokePlanner/Controls/Options.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel;
+using PokePlanner.Util;
+
+namespace PokePlanner.Controls
+{
+    /// <summary>
+    /// Interaction logic for Options.xaml.
+    /// </summary>
+    public partial class Options
+    {
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public Options()
+        {
+            InitializeComponent();
+
+            if (!DesignerProperties.GetIsInDesignMode(this))
+            {
+                grid.Background = MyBrushes.PastelBlue;
+            }
+        }
+    }
+}
