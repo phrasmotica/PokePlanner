fe67e7128aa13a64fb383b50738c97768eb7d4d2
Julian Lawrance <julian.lawrance@gmail.com>
2019-07-08 17:37:41 +0100

    Use local PokeAPIv2 instance for dev work

--- PokePlanner/App.xaml.cs
+++ PokePlanner/App.xaml.cs
@@ -1,7 +1,23 @@
-﻿namespace PokePlanner
+﻿using System.Windows;
+using PokeAPI;
+
+namespace PokePlanner
 {
     /// <summary>
     /// Interaction logic for App.xaml.
     /// </summary>
-    public partial class App { }
+    public partial class App
+    {
+#if DEBUG
+        /// <summary>
+        /// Set DataFetcher to use local instance of PokeAPI.
+        /// </summary>
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            DataFetcher.DataBackend = new HttpBackend("http://localhost:8000/api/v2/", "My fork of PokeAPI.NET!");
+        }
+#endif
+    }
 }
