c382fa16b7ad11380db7c7e28c3d9aec3aa8c6c2
Julian Lawrance <julian.lawrance@gmail.com>
2019-07-15 21:57:44 +0100

    Create class for caching data for the session

---
+++ PokePlanner/Util/SessionCache.cs
@@ -0,0 +1,25 @@
+﻿using PokeAPI;
+
+namespace PokePlanner.Util
+{
+    /// <summary>
+    /// Singleton for caching data during the application session.
+    /// </summary>
+    public class SessionCache
+    {
+        /// <summary>
+        /// The selected game's generation.
+        /// </summary>
+        public Generation Generation;
+
+        /// <summary>
+        /// Singleton constructor;
+        /// </summary>
+        private SessionCache() { }
+
+        /// <summary>
+        /// Singleton instance.
+        /// </summary>
+        public static SessionCache Instance { get; set; } = new SessionCache();
+    }
+}
