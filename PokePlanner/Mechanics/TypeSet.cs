cd0d31e6f38f7a9af82fbba35807ff98583b6936
Julian Lawrance <julian.lawrance@gmail.com>
2019-07-15 22:03:19 +0100

    Create types representing a set of Pokemon types in the type chart

---
+++ PokePlanner/Mechanics/TypeSet.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace PokePlanner.Mechanics
+{
+    /// <summary>
+    /// Represents a set of types used by a game.
+    /// </summary>
+    public class TypeSet : Dictionary<Type, ActiveInfo>
+    {
+        /// <summary>
+        /// Returns all active types.
+        /// </summary>
+        public Type[] ActiveTypes => Keys.Where(k => this[k].IsActive).ToArray();
+    }
+
+    /// <summary>
+    /// Represents the activity of a type in a type set.
+    /// </summary>
+    public struct ActiveInfo
+    {
+        public ActiveInfo(bool isActive, int column)
+        {
+            IsActive = isActive;
+            Column = column;
+        }
+
+        public bool IsActive { get; set; }
+        public int Column { get; set; }
+    }
+}
