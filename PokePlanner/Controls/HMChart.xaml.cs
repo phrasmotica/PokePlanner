54fb52f758297c43f0e6715165e67fd65fdaac3f
Julian Lawrance <julian.lawrance@gmail.com>
2019-07-21 20:13:11 +0100

    Create HM coverage panel

---
+++ PokePlanner/Controls/HMChart.xaml.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Windows;
+using PokePlanner.Util;
+
+namespace PokePlanner.Controls
+{
+    /// <summary>
+    /// Interaction logic for HMChart.xaml.
+    /// </summary>
+    public partial class HMChart
+    {
+        public HMChart()
+        {
+            InitializeComponent();
+        }
+    }
+}
