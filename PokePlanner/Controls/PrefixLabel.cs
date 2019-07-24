6e05965dc8b0687644b19c52f41f989ad0210ccf
Julian Lawrance <julian.lawrance@gmail.com>
2019-07-24 21:03:55 +0100

    Create control for a label with a string prefix

---
+++ PokePlanner/Controls/PrefixLabel.cs
@@ -0,0 +1,32 @@
+﻿using System.Windows.Controls;
+
+namespace PokePlanner.Controls
+{
+    /// <summary>
+    /// Label with a string prefix independent from its content.
+    /// </summary>
+    public class PrefixLabel : Label
+    {
+        /// <summary>
+        /// The prefix for the label.
+        /// </summary>
+        public string Prefix;
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public PrefixLabel(string prefix)
+        {
+            Prefix = prefix;
+            Content = Prefix;
+        }
+
+        /// <summary>
+        /// Method for setting the label text after the prefix.
+        /// </summary>
+        public void SetContent(string newContent)
+        {
+            Content = Prefix + newContent;
+        }
+    }
+}
