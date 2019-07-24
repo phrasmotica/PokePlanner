ea347b211e4f269f5f6b573dd7cb4a187be2b3ea
Julian Lawrance <julian.lawrance@gmail.com>
2019-07-24 21:04:58 +0100

    Create a prefix label for each HM slot in constructor

--- PokePlanner/Controls/HMChart.xaml.cs
+++ PokePlanner/Controls/HMChart.xaml.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.ComponentModel;
 using System.Windows;
 using PokePlanner.Util;
 
@@ -9,9 +11,33 @@ namespace PokePlanner.Controls
     /// </summary>
     public partial class HMChart
     {
+        /// <summary>
+        /// The number of HM items across all games.
+        /// </summary>
+        public const int NUM_HMS = 8;
+
+        /// <summary>
+        /// Collection of labels for displaying each HM.
+        /// </summary>
+        private readonly IList<PrefixLabel> hmLabels;
+
+        /// <summary>
+        /// Create a label for each HM move.
+        /// </summary>
         public HMChart()
         {
             InitializeComponent();
+
+            if (!DesignerProperties.GetIsInDesignMode(this))
+            {
+                hmLabels = new List<PrefixLabel>();
+                for (int i = 0; i < NUM_HMS; i++)
+                {
+                    var label = new PrefixLabel($@"HM{i + 1:D2}: ");
+                    panel.Children.Add(label);
+                    hmLabels.Add(label);
+                }
+            }
         }
     }
 }
