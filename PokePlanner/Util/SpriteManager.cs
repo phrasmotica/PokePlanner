262e697d78c26e16327c4292e4016ba8c5bb08f6
Julian Lawrance <julian.lawrance@gmail.com>
2019-07-27 20:57:18 +0100

    Create singleton for managing sprites

---
+++ PokePlanner/Util/SpriteManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace PokePlanner.Util
+{
+    /// <summary>
+    /// Singleton for managing Pokemon sprites.
+    /// </summary>
+    public class SpriteManager
+    {
+        /// <summary>
+        /// Local path for saving sprites.
+        /// </summary>
+        public string SpriteDir;
+
+        /// <summary>
+        /// Singleton constructor.
+        /// </summary>
+        private SpriteManager()
+        {
+            var appDataPath = Environment.GetEnvironmentVariable("LocalAppData") + @"\PokePlanner";
+            SpriteDir = appDataPath + @"\sprites";
+            Directory.CreateDirectory(SpriteDir);
+        }
+
+        /// <summary>
+        /// Singleton instance.
+        /// </summary>
+        public static SpriteManager Instance { get; set; } = new SpriteManager();
+    }
+}
