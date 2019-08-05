c9ca1b9a09b4f0945643358939e5b1f3d46f6282
Julian Lawrance <julian.lawrance@gmail.com>
2019-08-05 21:04:24 +0100

    Create class representing a team member

---
+++ PokePlanner/Util/TeamMember.cs
@@ -0,0 +1,48 @@
+﻿using System.Threading.Tasks;
+using PokeApiNet.Models;
+using PokePlanner.Properties;
+
+namespace PokePlanner.Util
+{
+    /// <summary>
+    /// Class representing a Pokemon on a team.
+    /// </summary>
+    public class TeamMember
+    {
+        /// <summary>
+        /// Whether the Pokemon is valid on the team.
+        /// </summary>
+        public bool IsValid;
+
+        /// <summary>
+        /// The slot that this team member occupies.
+        /// </summary>
+        public int Slot;
+
+        /// <summary>
+        /// Gets or sets the Pokemon.
+        /// </summary>
+        public Pokemon Pokemon
+        {
+            get => IsValid ? pokemon : null;
+            set { pokemon = value; }
+        }
+        private Pokemon pokemon;
+
+        /// <summary>
+        /// Returns this team member's name.
+        /// </summary>
+        public async Task<string> GetName()
+        {
+            return IsValid ? await Pokemon.GetName() : string.Empty;
+        }
+
+        /// <summary>
+        /// Validates this Pokemon.
+        /// </summary>
+        public async Task Validate()
+        {
+            IsValid = await Pokemon.IsValid(SessionCache.Instance.VersionGroup);
+        }
+    }
+}
