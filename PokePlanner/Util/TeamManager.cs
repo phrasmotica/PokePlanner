b2e00808a15ca91501f9e34b0c8f4f05370229ee
Julian Lawrance <julian.lawrance@gmail.com>
2019-08-05 19:53:50 +0100

    Create singleton for managing the selected team

---
+++ PokePlanner/Util/TeamManager.cs
@@ -0,0 +1,18 @@
+﻿namespace PokePlanner.Util
+{
+    /// <summary>
+    /// Class for managing the selected team of Pokemon.
+    /// </summary>
+    public class TeamManager
+    {
+        /// <summary>
+        /// Singleton constructor.
+        /// </summary>
+        private TeamManager() { }
+
+        /// <summary>
+        /// Singleton instance.
+        /// </summary>
+        public static TeamManager Instance { get; set; } = new TeamManager();
+    }
+}
